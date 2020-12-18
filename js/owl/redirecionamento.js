b9cb732a91697cd714312b1b7d32a9dc40be46a1
yostto-leal <yosttoleal@gmail.com>
2020-12-18 08:23:02 -0300

    funcionalidade dos botoes

---
+++ js/owl/redirecionamento.js
@@ -0,0 +1,7 @@
+function AssistirClip (){
+    window.location.href = "https://www.youtube.com/watch?v=cnqV3wsZlpo" ;
+}
+
+function Info (){
+    window.location.href = "https://www.themoviedb.org/tv/87739-the-queen-s-gambit?language=pt-BR";
+}
